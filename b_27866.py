a31c0bc13747bd61eaa1bc576fcd0af96fab78e4
GEUNHYEONG <gim35182@g.yju.ac.kr>
2026-03-14 19:17:18 +0900

    문자열 원하는 문자 출력

---
+++ b_27866.py
@@ -0,0 +1,24 @@
+# 단어 입력받기
+world = input()
+
+# 숫자 입력받기
+num = int(input())
+
+# 반복문으로 입력받은 숫자 기입
+for _ in range(num):
+    _
+
+# 출력
+print(world[_])
+
+# 인덱스 이용해보기
+# world = list(input())
+# num = int(input())
+# print(world[num-1])
+
+
+
+
+
+
+
